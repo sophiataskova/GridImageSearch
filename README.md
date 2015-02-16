9accda499273e13891a10c3f19f9f61977bbc0c7
Sofiya Taskova <sofiya@hipmunk.com>
2015-02-15 20:28:57 -0800

    added share capability, updated read me

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # GridImageSearch
+
+An app for searching for images on google, with a few filter options
+
+- User can enter a search query that will display a grid of image results from the Google Image API.
+- User can click on "settings" which allows selection of advanced search options to filter results
+- User can configure advanced search filters such as:
+  - Size (small, medium, large, extra-large)
+  - Color filter (black, blue, brown, gray, green, etc...)
+  - Type (faces, photo, clip art, line art)
+  - Site (espn.com)
+- Subsequent searches will have any filters applied to the search results
+- User can tap on any image in results to see the image full-screen
+- User can scroll down “infinitely” to continue loading more image results (up to 8 pages)
+- Checks if internet is available
+- User can share an image to their friends or email it to themselves
+- Used the StaggeredGridView to display improve the grid of image results
+
+This took about 8 hours to write. 
+
+See gif walkthrough at ![Video Walkthrough](https://s3.amazonaws.com/uploads.hipchat.com/20599/752135/4hUD1ilHtxuSTGS/image_search_demo.gif)
+
+Gif of behavior when all results are filtered out: ![Video Walkthrough 2](https://s3.amazonaws.com/uploads.hipchat.com/20599/752135/mvTeok9EACKZlrF/image_search_demo_no_results.gif)
--- app/src/main/java/com/example/sophiataskova/gridimagesearch/activities/ImageDisplayActivity.java
+++ app/src/main/java/com/example/sophiataskova/gridimagesearch/activities/ImageDisplayActivity.java
@@ -1,24 +1,37 @@
 package com.example.sophiataskova.gridimagesearch.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.example.sophiataskova.gridimagesearch.R;
 import com.example.sophiataskova.gridimagesearch.models.ImageResult;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ImageDisplayActivity extends Activity {
+
+    private ImageView ivResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_display);
-        getActionBar().hide();
+//        getActionBar().hide();
         ImageResult result = getIntent().getParcelableExtra("url");
-        ImageView ivResult = (ImageView)findViewById(R.id.iv_image);
+        ivResult = (ImageView)findViewById(R.id.iv_image);
         Picasso.with(this).load(result.fullUrl).into(ivResult);
     }
 
@@ -38,10 +51,55 @@ public class ImageDisplayActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_share) {
+            onShareItem(ivResult);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onShareItem(View v) {
+        // Get access to bitmap image from view
+        ImageView ivImage = (ImageView) findViewById(R.id.iv_image);
+        // Get access to the URI for the bitmap
+        Uri bmpUri = getLocalBitmapUri(ivImage);
+        if (bmpUri != null) {
+            // Construct a ShareIntent with link to image
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+            shareIntent.setType("image/*");
+            // Launch sharing dialog for image
+            startActivity(Intent.createChooser(shareIntent, "Share Image"));
+        } else {
+            // ...sharing failed, handle error
+        }
+    }
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+
 }
